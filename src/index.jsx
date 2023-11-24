53f09848c04a2d39f2735910ebfe1679849cf47c
Umang Shrestha <umangstha124@gmail.com>
2023-11-24 14:03:27 +0545

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div style={{ backgroundColor: "black", color: "grey" }}>
+    <Header />
+  </div>
+);
