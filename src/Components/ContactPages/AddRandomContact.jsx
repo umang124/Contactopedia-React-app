097cf98aac5f88bb41caf8790005f6fc8952a185
Umang Shrestha <umangstha124@gmail.com>
2023-11-26 13:17:07 +0545

    Adding Random Contact using Axios Call

--- src/Components/ContactPages/AddRandomContact.jsx
+++ src/Components/ContactPages/AddRandomContact.jsx
@@ -1,7 +1,23 @@
-const AddRandomContact = () => {
+import { getRandomUser } from "../../Utility/api";
+
+const GetRandomContact = async (props) => {
+  const responseFromAPI = await getRandomUser();
+
+  return props.handleAddRandomContact({
+    name:
+      responseFromAPI.data.first_name + " " + responseFromAPI.data.last_name,
+    email: responseFromAPI.data.email,  
+    phone: responseFromAPI.data.phone_number,
+  });
+};
+
+const AddRandomContact = (props) => {
   return (
     <div>
-      <button className="btn btn-danger form-control">
+      <button
+        className="btn btn-success form-control"
+        onClick={() => GetRandomContact(props)}
+      >
         Add Random Contact
       </button>
     </div>
